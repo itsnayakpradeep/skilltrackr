a8c00fd5d7b6729dcea1263ffc00026aebb82ee2
Pradeep Nayak <pradeepnayak274@gmail.com>
2025-08-12 08:41:01 +0530

    Navigation list has been added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { colors } from "@styles/token/colors";
+import { typography } from "@styles/token/typography";
 import Button from "@components/ui/Button";
 
 export default function Navbar() {
@@ -8,21 +10,42 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16 px-4">
                 {/* Logo */}
-                <div className="bg-white flex-shrink-0">
-                    <Link href="/" className="flex items-center">
-                        <Image
-                            src="/SkillTrackrlogo.svg"
-                            alt="SkillTrackr-Logo"
-                            width={170}
-                            height={50}
-                            className="h-10 w-10"
-                        />
-                    </Link>
-                    
+                    <div className="bg-white flex-shrink-0">
+                        <Link href="/" className="flex items-center">
+                            <Image
+                                src="/SkillTrackrlogo.svg"
+                                alt="SkillTrackr-Logo"
+                                width={170}
+                                height={50}
+                                className="h-10 w-10"
+                            />
+                        </Link>
+                        
+                    </div>
+                {/* Navigation Links*/}
+                <div className="flex items-center ml-auto">
+                    <ul className="flex items-center list-none mx-6 px-4 space-x-8">
+                        <li>
+                            <Link  
+                                href="/features" 
+                                className="font-sans text-gray-700 hover:text-primary font-medium no-underline"
+                            >
+                                Features
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/testimonial" className="text-gray-700 hover:text-primary font-medium no-underline">
+                                Testimonial
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/price" className="text-gray-700 hover:text-primary font-medium no-underline">
+                                Price
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-            {/* Navigation Links */}
-            
-            {/* Auth Buttons */}
+                {/* Auth Buttons */}
                 <div className="inline-flex rounded-md shadow-sm" role="group">
                     {/* <Link
                         href="/login"
